39a417e859ae5f7a37e587341c17e27280f210cc
DAVID LUCAS DOS SANTOS <25132827@CFP127ALUNO.LOCAL>
2025-04-02 17:12:18 -0300

    Tabuada Concluida

---
+++ src/br/dev/david/tabuada/model/Tabuada.java
@@ -0,0 +1,48 @@
+package br.dev.david.tabuada.model;
+
+public class Tabuada {
+
+	private double multiplicando;
+	private double minimoMultiplicador;
+	private double maximoMultiplicador;
+
+	
+	public void setMultiplicando(double multiplicando) {
+		this.multiplicando = multiplicando;
+	}
+	public double getMultiplicando() {
+		return multiplicando;
+	}
+
+	
+	public void setMinimoMultiplicador(double minimoMultiplicador) {
+		this.minimoMultiplicador = minimoMultiplicador;
+	}
+	public double getMinimoMultiplicador() {
+		return minimoMultiplicador;
+	}
+
+	
+	public void setMaximoMultiplicador(double maximoMultiplicador) {
+		this.maximoMultiplicador = maximoMultiplicador;
+	}
+	public double getMaximoMultiplicador() {
+		return maximoMultiplicador;
+	}
+
+	
+	public void mostrarTabuada() {
+		
+		double contador = minimoMultiplicador;
+		while (contador <= maximoMultiplicador) {
+			double produto = multiplicando * contador;
+			System.out.println(multiplicando + " X " + contador + " = " + produto);
+			contador = contador + 1;
+		}
+		
+		System.out.println("-------------------------");
+		System.out.println("");
+		
+		
+	}
+}
